68907f2c991dbbe1afc4461eede8345e9df7888a
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2015-03-10 14:51:47 +0100

    remove unecessary storage from deep/branch and deep/OrderedBranch

--- deep/Branch.js
+++ deep/Branch.js
@@ -5,18 +5,14 @@ var _ContentDelegate = require('../_ContentDelegate');
 var bindBranch = require('ksf/observable/deep/bindBranch');
 
 module.exports = compose(_Destroyable, _ContentDelegate, function(args) {
-	var storage = {}; // allow to store things by key (mainly for canceling observations)
 	this._content = args.content;
 
 	this._own(bindBranch(args.value, {
 		add: function(keyAdded) {
-			var ret = args.onKeyAdded(keyAdded, args.content);
-			if (ret) {
-				storage[keyAdded] = ret;
-			}
+			return args.onKeyAdded(keyAdded, args.content);
 		},
-		remove: function(keyRemoved) {
-			args.onKeyRemoved(keyRemoved, args.content, storage[keyRemoved]);
+		remove: function(keyRemoved, storedReturn) {
+			args.onKeyRemoved(keyRemoved, args.content, storedReturn);
 		},
 	}));
 });
--- deep/OrderedBranch.js
+++ deep/OrderedBranch.js
@@ -5,19 +5,17 @@ var _ContentDelegate = require('../_ContentDelegate');
 var bindOrdered = require('ksf/observable/deep/bindOrderedBranch');
 
 module.exports = compose(_Destroyable, _ContentDelegate, function(args) {
-	var storage = {}; // allow to store things by key (mainly for canceling observations)
 	this._content = args.content;
 
 	this._own(bindOrdered(args.value, {
 		add: function(keyAdded, beforeKey) {
-			var ret = args.onKeyAdded(args.content, keyAdded, beforeKey);
-			storage[keyAdded] = ret;
+			return args.onKeyAdded(args.content, keyAdded, beforeKey);
 		},
 		move: function(key, beforeKey) {
 			args.onKeyMoved(args.content, key, beforeKey);
 		},
-		remove: function(keyRemoved) {
-			args.onKeyRemoved(args.content, keyRemoved, storage[keyRemoved]);
+		remove: function(keyRemoved, storedReturn) {
+			args.onKeyRemoved(args.content, keyRemoved, storedReturn);
 		},
 	}));
 });
