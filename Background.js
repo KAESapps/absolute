5e7b03cd83fc4375f88d44dc66cb99b59dc9dfe6
Quentin <quentin.vuilliot@kaes.fr>
2015-03-16 10:55:07 +0100

    Light changes

--- Background.js
+++ Background.js
@@ -3,6 +3,7 @@ var _Destroyable = require('ksf/base/_Destroyable');
 var Elmt = require('./Element');
 var ZPile = require('./ZPile');
 var _ContentDelegate = require('./_ContentDelegate');
+var capitalize = require('lodash/string/capitalize');
 
 module.exports = compose(_Destroyable, _ContentDelegate, function(content) {
 	this._bg = new Elmt();
@@ -16,7 +17,13 @@ module.exports = compose(_Destroyable, _ContentDelegate, function(content) {
 		return this;
 	},
 	border: function(border) {
-		this._bg.styleProp('border', border);
+		if (typeof border === 'string') {
+			this._bg.styleProp('border', border);
+		} else {
+			for (var p in border) {
+				this._bg.styleProp('border' + capitalize(p), border[p]);
+			}
+		}
 		return this;
 	},
 });
--- layout/MasterFull.js
+++ layout/MasterFull.js
@@ -2,13 +2,14 @@ var compose = require('ksf/utils/compose');
 var _Destroyable = require('ksf/base/_Destroyable');
 var Full = require('./Full');
 var delegateGetSet = require('../utils/delegateGetSet');
+var capitalize = require('lodash/string/capitalize');
 
 module.exports = compose(_Destroyable, function(axis, master) {
 	this._master = master;
 	var contentLayout = this._contentLayout = new Full(axis);
 	
 	var sizeProp = this._sizeProp = (axis === 'vertical' ? 'height' : 'width');
-	var onSizeMethod = 'on' + sizeProp[0].toUpperCase() + sizeProp.substring(1);
+	var onSizeMethod = 'on' + capitalize(sizeProp);
 	
 	contentLayout.size(master[sizeProp]());
 	this._own(master[onSizeMethod](function(size) {
