1ad5a59325ea2843a0a12ac035488524be72a622
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2015-07-31 15:02:19 +0200

    Check Full.add if key already used

--- layout/Full.js
+++ layout/Full.js
@@ -15,6 +15,10 @@ module.exports = compose(function(axis) {
 		return this;
 	},
 	add: function(key, cmp) {
+		// TODO: remove check in build?
+		if (key in this._children) {
+			throw 'key already used';
+		}
 		this._children[key] = cmp;
 		this._applySize(key);
 		this._applyPosition(key);
