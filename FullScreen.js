f8663cc67c8a1e5aeb91b5799640052c22f4f9b3
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2015-08-13 11:42:44 +0200

    prevent Fullscreen for systematically call 'width' et 'height' on resize

--- FullScreen.js
+++ FullScreen.js
@@ -10,7 +10,30 @@ var _ContentDelegate = require('./_ContentDelegate');
 
 // pilote un composant pour le mettre en plein écran et réagir au redimensionnement
 module.exports = compose(_Destroyable, _ContentDelegate, function(content) {
-	sizePage(content);
-	content.left(0).top(0).zIndex(0).parentNode(document.body).containerVisible(true);
-	window.addEventListener('resize', sizePage.bind(null, content));
+	var currentWidth, currentHeight
+	// init
+	content
+		.left(0)
+		.top(0)
+		.zIndex(0)
+		.width(currentWidth = window.innerWidth)
+		.height(currentHeight = window.innerHeight)
+		.parentNode(document.body)
+		.containerVisible(true)
+	// update
+	// TODO: limit execution rate at 60fps ?
+	var resizeCb = function () {
+		var newWidth = window.innerWidth
+		var newHeight = window.innerHeight
+		if (newWidth !== currentWidth) {
+			content.width(currentWidth = newWidth)
+		}
+		if (newHeight !== currentHeight) {
+			content.height(currentHeight = newHeight)
+		}
+	}
+	window.addEventListener('resize', resizeCb)
+	this._own(function () {
+		window.removeEventListener('resize', resizeCb)
+	})
 });
