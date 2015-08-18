20f110fbfb30278d72509fbf3fa8efb2856de46f
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2015-08-18 22:24:11 +0200

    make 'onKeyMoved' optionnal

--- deep/OrderedBranch.js
+++ deep/OrderedBranch.js
@@ -7,15 +7,19 @@ var bindOrdered = require('ksf/observable/deep/bindOrderedBranch');
 module.exports = compose(_Destroyable, _ContentDelegate, function(args) {
 	this._content = args.content;
 
-	this._own(bindOrdered(args.value, {
+	var cbs = {
 		add: function(keyAdded, beforeKey) {
 			return args.onKeyAdded(args.content, keyAdded, beforeKey);
-		},
-		move: function(key, beforeKey) {
-			args.onKeyMoved(args.content, key, beforeKey);
 		},
 		remove: function(keyRemoved, storedReturn) {
 			args.onKeyRemoved(args.content, keyRemoved, storedReturn);
 		},
-	}));
+	}
+	if (args.onKeyMoved) {
+		cbs.move = function(key, beforeKey) {
+			args.onKeyMoved(args.content, key, beforeKey);
+		}
+	}
+
+	this._own(bindOrdered(args.value, cbs));
 });
